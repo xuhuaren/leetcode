48acce75e1c754e42e0ff008d9b6a24707500a72
Xuhua Ren <renxuhua@sjtu.edu.cn>
2019-05-07 21:59:59 +0800

    easy to understand

--- algorithms/cpp/twoSum/twoSum.cpp
+++ algorithms/cpp/twoSum/twoSum.cpp
@@ -82,3 +82,31 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> m;
+        vector<int> result;
+        for (int i=0; i < nums.size(); i++)
+        {
+            int NumberFind = target - nums[i];
+            // if numberfind is found in map, return them
+            
+            if (m.find(NumberFind) != m.end())
+            {
+                result.push_back(m[NumberFind]);
+                result.push_back(i);
+                return result;
+                
+            }
+            m[nums[i]] = i;
+            
+        }
+        
+        return result;
+        
+    }
+};
