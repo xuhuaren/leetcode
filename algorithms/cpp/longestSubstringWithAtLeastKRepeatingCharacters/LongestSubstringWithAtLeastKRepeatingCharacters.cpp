ae91dabbfb3f39da674060dfd3515746441bfce2
Xuhua Ren <renxuhua@sjtu.edu.cn>
2019-05-08 11:17:33 +0800

    Update LongestSubstringWithAtLeastKRepeatingCharacters.cpp

--- algorithms/cpp/longestSubstringWithAtLeastKRepeatingCharacters/LongestSubstringWithAtLeastKRepeatingCharacters.cpp
+++ algorithms/cpp/longestSubstringWithAtLeastKRepeatingCharacters/LongestSubstringWithAtLeastKRepeatingCharacters.cpp
@@ -107,3 +107,26 @@ private:
         return elems;
     }
 };
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int res = 0, left = -1, n = s.size();
+        unordered_map<int, int> m;
+        for (int i=0; i<n; ++i)
+        {
+            if (m.count(s[i]) && m[s[i]] > left) 
+            {
+                left = m[s[i]];
+            }   
+            
+            m[s[i]] = i;
+            res = max(res, i-left);
+        }
+        
+        return res;
+        
+    }
+};
